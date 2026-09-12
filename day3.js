4a9110669311c0d11813617626d8150c9258f4c6
Balathilaga <yuganviswa@gmail.com>
2026-09-12 22:41:28 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,266 @@
+ let n=1;
+while(n<=20){
+        console.log(n);
+        n++;
+    }
+  
+   console.log("this is do while loop")
+    let i = 1;
+    do {
+        console.log(i);
+        i++;
+     } while (i <= 15);
+
+
+     console.log("this is for loop")
+     for(a=1;a<=20;a++){
+        console.log(a);
+     }
+
+    console.log("QUESTION NO 2 odd number")
+     let ba = 1;
+     while(ba<=20){
+        console.log(ba);
+        ba +=2;
+     }
+
+     let ca=1;
+     do{
+        consol.log(ca);
+        ca += 2 ;
+     } while(ca<=20);
+ 
+     let e = 1
+     for(e=1;e<=20;e+=2){
+        console,log(b);
+     }
+
+
+     console.log("QUESTION 3 even number")
+     let d = 0;
+     while(d <= 20){
+        console.log(d)
+        d +=2;
+     }
+
+     let f = 0;
+     do{
+        console.log(f);
+        f +=2;
+     }while(f<=20);
+      
+    let g = 0;
+    for(g=0;g<=20;g+=2){
+        console.log(g);
+    } 
+
+    console.log( "QUESTION NO 4");
+
+    let five = 0;
+    while(five<=20){
+        console.log(five);
+        five += 5;
+    }
+ 
+    let h = 0 ;
+    do{
+        consol.log(h);
+        h += 5;
+    }while(h<=20);
+
+    let j = 0;
+    for(j = 0;j<=20;j+=5){
+        console.log(j);
+    }
+
+   console.log("question no 5");
+
+ let result = "" ;
+ for(let i=1;i<=4;i++){
+   result += i;
+   if(i<=4){
+       result+="+";
+   }
+}
+ console.log(result);
+
+ let s=""
+ let c=1;
+ while(c<=4){
+   s += c; 
+   if(c <= 4){
+      s +="+"
+   }
+   c++;
+ }
+   console.log(s);
+
+   let q="";
+   let w=1;
+   do {
+      q += w;
+      if(w<=4){
+         q +="+";
+      }
+      w++;
+   }while(w<=4);
+   console.log(q);
+
+   
+console.log("quetion no 6");
+   let p=""
+   for(let u=0;u<=6;u += 2){
+       p += u;
+       if(u<=6){
+         p += "+";
+       }
+   }console.log(p);
+
+   let sum ="";
+   let of = 0;
+   while(of <=6){
+      sum += of;
+      if(of<=6){
+         sum += "+";
+      }
+      of += 2;
+   }console.log(sum);
+
+   let bala = "";
+   let thilaga = 0;
+   do{
+      bala =bala + thilaga;
+      if(thilaga<=6){
+         bala =bala + "+";
+      } 
+      thilaga += 2;
+   }while(thilaga<=6);
+   console.log(bala);
+
+  
+console.log("quetion no 6");
+   let girl = "";
+   for(let frnd=1;frnd<=9;frnd = frnd+2){
+      girl =girl + frnd;
+      if(frnd<9){
+         girl = girl +"+";
+      }
+   }console.log(girl);
+
+   let t=1;
+   let b="";
+   while(t<=9){
+      b = b + t;
+      if(t<9){
+         b = b + "+";
+      }
+      t +=2;
+   }console.log(b);
+
+   let sara ="";
+   let sam = 1;
+   do{
+      sara =sara + sam;
+      if(sam<=9){
+         sara = sara + "+";
+      }
+      sam +=2;
+   }while(sam<=9);
+   console.log(sara);
+   
+console.log("question no 9");
+ let r = "" ;  
+for(let i=1;i<=4;i++){
+   r += i+"/" +i+"!";
+   if(i<4){
+       r +="+";
+   }
+}
+ console.log(r);
+
+ let bun = "";
+ let jam = 1;
+ while(jam<=4){
+   bun =bun + jam+"/"+jam+"!";
+   if(jam<4){
+        bun =bun +"+";
+   }
+   jam++;
+ }console.log(bun);
+
+ let gun="";
+ let shot=1;
+ do{
+   gun =gun + shot + "/" +shot +"!";
+   if(shot<4){
+    gun = gun + "+";
+   }
+    shot++;
+ }while(shot<=4);
+ console.log(gun);
+
+//  console.log(Question10);
+
+ let rose = "" ;  
+for(let i=1;i<=5;i= i+2){
+   rose += i+"/" +i+"!";
+   if(i<5){
+       rose +="+";
+   }
+}
+ console.log(rose);
+
+ 
+ let bo = "";
+ let ja = 1;
+ while(ja<=5){
+   bo =bo + ja+"/"+ja+"!";
+   if(ja<5){
+        bo =bo +"+";
+   }
+   ja += 2;
+ }console.log(bo);
+
+ let gu="";
+ let sho=1;
+ do{
+   gu =gu + sho + "/" +sho +"!";
+   if(sho<5){
+    gu =gu + "+";
+   }
+    sho += 2;
+ }while(sho<=5);
+ console.log(gu);
+  
+
+//  console.log(Qestion11);
+ let ros = "" ;  
+for(let i=0;i<=6;i= i+2){
+   ros += i+"/" +i+"!";
+   if(i<6){
+       ros +="+";
+   }
+}
+ console.log(ros);
+
+ 
+ let boo = "";
+ let jaa = 0;
+ while(jaa<=6){
+   boo =boo + jaa+"/"+jaa+"!";
+   if(jaa<6){
+        boo =boo +"+";
+   }
+   jaa += 2;
+ }console.log(boo);
+
+ let guu="";
+ let shoo=0;
+ do{
+   guu =guu + shoo + "/" +shoo +"!";
+   if(shoo<6){
+    guu =guu + "+";
+   }
+    shoo += 2;
+ }while(shoo<=6);
+ console.log(guu);
